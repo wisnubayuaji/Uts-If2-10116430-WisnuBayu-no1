81170a1f19e383899d890059817106205cba917a
wisnubayuaji <wisnubayu34@gmail.com>
2020-12-04 22:10:59 +0700

    Initial commit

---
+++ src/Age.java
@@ -0,0 +1,32 @@
+public class Age {
+    private int yearBirth;
+    private int yearNow;
+    private String red;
+
+    public Age(int yearNow) {
+
+    }
+    public int getYearBirth() {
+        return yearBirth;
+
+    }
+    
+    public void setYearBirth(int yearBirth) {
+        System.out.println("");
+    }
+    
+    public int getYearNow() {
+		return yearNow;
+
+    }
+
+    public int hitungUmur() {
+        return yearNow;
+
+    }
+
+    public String tandanyaKamu(int umur) {
+        return red;
+
+    }
+}
